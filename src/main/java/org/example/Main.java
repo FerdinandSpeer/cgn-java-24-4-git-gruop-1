f1ab2d6a5b7fc0670a1ff9fbbcda44301774684f
ahmadad62 <a.naghian@gmail.com>
2024-08-16 16:09:41 +0200

    first Ahmad commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Das ist ein erster Test!");
         System.out.println("Hello world!");
+        System.out.println("Hello Ferdinand!");
     }
 }

1965bef37181f93eda11fa983c671c2faa08d49a
ferdinandspeer <ferdinandspeer@gmail.com>
2024-08-16 16:17:24 +0200

    new commit hello Ahmad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Das ist ein erster Test!");
         System.out.println("Hello world!");
         System.out.println("Hello Ferdinand!");
+        System.out.println("Hello Ahmad, it works!");
     }
 }

64956e3cbca58705900e50b34e9fb9ca38f3d8e3
ferdinandspeer <ferdinandspeer@gmail.com>
2024-08-16 15:58:12 +0200

    Add sout Test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Das ist ein erster Test!");
+        System.out.println("Hello world!");
+    }
+}

657a694ea4ebbaabe5ec429e5277f875cecb8c57
ahmadad62 <a.naghian@gmail.com>
2024-08-19 11:54:14 +0200

    step5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,33 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-      int [] coinbox={1,3232,445454,65652,2,3};
-      int [] coinbox1=new int[5];
-      for (int i=0;i<coinbox1.length;i++){
-//          System.out.println(coinbox[i]);
-          System.out.println(coinbox1[i]);
-      }
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] numbers2 = {11, 12, 13,14, 15, 16, 17, 18, 19, 20};
+        int[] numbers3=new int[numbers.length];
+        int sum = 0;
+        int max = numbers[0];  // Initialize max with the smallest possible integer value
 
-      for (int coin : coinbox){       //for each loop
-          System.out.println(coin);
-      };
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];  // Accumulate the sum
+            System.out.println("Current sum: " + sum);
+
+            // Update max if the current number is greater than the current max
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            System.out.println("Current max: " + max);
+        }
+
+        // Output the final results
+        System.out.println("Final sum: " + sum);
+        System.out.println("Final max: " + max);
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers3[i] = numbers[i]+numbers2[i];
+        }
+        System.out.println(Arrays.toString(numbers3));
     }
 }

00a08d3a8259a87e9cb714a03e6706207c827d41
ahmadad62 <a.naghian@gmail.com>
2024-08-19 10:19:45 +0200

    array and for

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Das ist ein erster Test!");
-        System.out.println("Hello world!");
-        System.out.println("Hello Ferdinand!");
-        System.out.println("Hello Ahmad, it works!");
+      int [] coinbox={1,3232,445454,65652,2,3};
+      int [] coinbox1=new int[5];
+      for (int i=0;i<coinbox1.length;i++){
+//          System.out.println(coinbox[i]);
+          System.out.println(coinbox1[i]);
+      }
+
+      for (int coin : coinbox){       //for each loop
+          System.out.println(coin);
+      };
     }
 }
